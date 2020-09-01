f7d1e3d8b51c98fd9a56edd93dcb89514767f09d
kevacoin <info@kevacoin.org>
2020-09-01 10:44:32 -0700

    Added padding top to empty messages.

--- screen/data/namespaces.js
+++ screen/data/namespaces.js
@@ -520,7 +520,7 @@ class MyNamespaces extends React.Component {
             }}
           />
           :
-          <ScrollView style={{flex: 1, paddingHorizontal: 5}}
+          <ScrollView style={{flex: 1, paddingHorizontal: 5, paddingTop: 30}}
             contentContainerStyle={{justifyContent: 'center', alignItems: 'center'}}
             refreshControl={
               <RefreshControl onRefresh={() => this.refreshNamespaces()} refreshing={this.state.isRefreshing} />
@@ -726,7 +726,7 @@ class OtherNamespaces extends React.Component {
           }}
         />
         {otherNamespaceList.order.length == 0 &&
-          <ScrollView style={{flex: 1, paddingHorizontal: 5}}
+          <ScrollView style={{flex: 1, paddingHorizontal: 5, paddingTop: 30}}
             contentContainerStyle={{justifyContent: 'center', alignItems: 'center'}}
             refreshControl={
               <RefreshControl onRefresh={() => this.refreshNamespaces()} refreshing={this.state.isRefreshing} />
